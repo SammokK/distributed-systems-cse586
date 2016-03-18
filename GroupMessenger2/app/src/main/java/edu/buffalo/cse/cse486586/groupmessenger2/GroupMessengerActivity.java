bf8681838c09132bf86d313cc4e3b7b4795dc3ab
SammokKabasi <sammokwhoelse@gmail.com>
2016-03-17 21:31:38 -0400

    Got serialization to work

--- GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
+++ GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
@@ -17,10 +17,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -31,25 +34,36 @@ import java.util.concurrent.PriorityBlockingQueue;
  * GroupMessengerActivity is the main Activity for the assignment.
  *
  * @author stevko
- *
  */
 public class GroupMessengerActivity extends Activity {
     static final String TAG = GroupMessengerActivity.class.getSimpleName();
-    private final ContentValues mContentValues =  new ContentValues();
+    private final ContentValues mContentValues = new ContentValues();
 
-    static final String REMOTE_PORTS[] = {"11108", "11112", "11116" , "11120" , "11124"};
+    static final String REMOTE_PORTS[] = {"11108", "11112", "11116", "11120", "11124"};
+
+    Socket sockets[] = new Socket[5];
+
+    String myPort = null;
+
     private final Uri mUri = buildUri("content", "edu.buffalo.cse.cse486586.groupmessenger2.provider");
 
     static final int SERVER_PORT = 10000;
     private ContentResolver mContentResolver;
 
-    long sequence = 0;
+    double sequence = 0;
 
     private PriorityBlockingQueue<MessageWrapper> queue = new PriorityBlockingQueue<MessageWrapper>(5, MessageWrapper.messageWrapperComparator);
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mContentResolver= getContentResolver();
+
+
+        mContentResolver = getContentResolver();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_messenger);
 
@@ -59,14 +73,14 @@ public class GroupMessengerActivity extends Activity {
          */
         TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
-        
+
         /*
          * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
          * OnPTestClickListener demonstrates how to access a ContentProvider.
          */
         findViewById(R.id.button1).setOnClickListener(
                 new OnPTestClickListener(tv, getContentResolver()));
-        
+
         /*
          * TODO: You need to register and implement an OnClickListener for the "Send" button.
          * In your implementation you need to get the message from the input box (EditText)
@@ -74,7 +88,7 @@ public class GroupMessengerActivity extends Activity {
          */
 
         final EditText editText = (EditText) findViewById(R.id.editText1);
-        Button sendButton  = (Button) findViewById((R.id.button4));
+        Button sendButton = (Button) findViewById((R.id.button4));
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,7 +113,7 @@ public class GroupMessengerActivity extends Activity {
          */
         TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
         String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
-        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+        myPort = String.valueOf((Integer.parseInt(portStr) * 2));
 
         try {
             /*
@@ -113,14 +127,6 @@ public class GroupMessengerActivity extends Activity {
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
         } catch (IOException e) {
-            /*
-             * Log is a good way to debug your code. LogCat prints out all the messages that
-             * Log class writes.
-             *
-             * Please read http://developer.android.com/tools/debugging/debugging-projects.html
-             * and http://developer.android.com/tools/debugging/debugging-log.html
-             * for more information on debugging.
-             */
             Log.e(TAG, "Can't create a ServerSocket");
             return;
         }
@@ -171,6 +177,9 @@ public class GroupMessengerActivity extends Activity {
                 return false;
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -178,6 +187,46 @@ public class GroupMessengerActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
         return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GroupMessenger Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://edu.buffalo.cse.cse486586.groupmessenger2/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GroupMessenger Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://edu.buffalo.cse.cse486586.groupmessenger2/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 
 
@@ -206,17 +255,57 @@ public class GroupMessengerActivity extends Activity {
              * TODO: Fill in your server code that receives messages and passes them
              * to onProgressUpdate().
              */
-
             try {
-                while(true) {
-                    Socket socket = serverSocket.accept();
-                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                while (true) {
+                    Log.v(TAG, "Waiting for next socket connection...");
+                    final Socket socket;
+                    socket = serverSocket.accept();
+                    Log.v(TAG, "Accepted socket" +
+                            socket);
+                    //create a new thread that keeps listening on loop
 
-                    String line = dataInputStream.readUTF();
-                    publishProgress(line);
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (true) {
+                                try {
+                                    ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                                    MessageWrapper wrapper = (MessageWrapper) objectInputStream.readObject();
+
+                                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+
+                                    if(wrapper.getType()==MessageWrapper.TYPE_MESSAGE) {
+                                        Log.v(TAG, "Message received with content-> " + wrapper.getData());
+
+                                        //check if message is insertable, if it is - insert to database
+
+                                        if(wrapper.isReady()) {
+                                            mContentValues.put(Constants.KEY, new StringBuffer().append(wrapper.getPriority()).toString());
+                                            mContentValues.put(Constants.VALUE, wrapper.getData());
+                                            mContentResolver.insert(mUri, mContentValues);
+                                        } else {
+                                            MessageWrapper reply = new MessageWrapper(null, sequence, MessageWrapper.TYPE_REPLY);
+                                            objectOutputStream.writeObject(reply);
+                                            Log.v(TAG, "Sent a reply with  " + reply);
+                                        }
+
+                                        sequence = wrapper.getPriority();
+                                    }
+                                    publishProgress(wrapper.toString());
+                                    Log.v(TAG, "Received message " + wrapper.getData() + "from socket " + socket);
+                                } catch (IOException e) {
+                                    Log.e(TAG, Log.getStackTraceString(e));
+                                } catch (ClassNotFoundException e) {
+                                    Log.e(TAG, Log.getStackTraceString(e));
+                                }
+                            }
+                        }
+                    });
+                    thread.start();
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                Log.e(TAG, Log.getStackTraceString(e));
             }
 
             return null;
@@ -233,10 +322,7 @@ public class GroupMessengerActivity extends Activity {
             TextView localTextView = (TextView) findViewById(R.id.textView1);
             localTextView.append("\n");
 
-            mContentValues.put(Constants.KEY, new StringBuffer().append(sequence++).toString());
-            mContentValues.put(Constants.VALUE, strings[0]);
 
-            mContentResolver.insert(mUri, mContentValues);
 
             /*
              * The following code creates a file in the AVD's internal storage and stores a file.
@@ -251,10 +337,10 @@ public class GroupMessengerActivity extends Activity {
             } catch (Exception e) {
                 Log.e(TAG, "File write failed");
             }
-
             return;
         }
     }
+
 
     /***
      * ClientTask is an AsyncTask that should send a string over the network.
@@ -267,31 +353,65 @@ public class GroupMessengerActivity extends Activity {
 
         @Override
         protected Void doInBackground(String... msgs) {
-            for (String a : REMOTE_PORTS) {
 
+            synchronized (sequence) {
+                //initializing each socket only once
                 try {
-
-
-                    Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
-                            Integer.parseInt(a));
-//TODO
-                    String msgToSend = msgs[0];
-                    OutputStream outputStream = socket.getOutputStream();
-                    DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-                    dataOutputStream.writeUTF(msgToSend);
-
-                    socket.close();
-
-                }catch(UnknownHostException e){
-                    Log.e(TAG, "ClientTask UnknownHostException");
-                }catch(IOException e){
-                    Log.e(TAG, "ClientTask socket IOException");
+                    for (int i = 0; i < REMOTE_PORTS.length; i++) {
+                        if (sockets[i] == null) {
+                            Log.v(TAG, "Creating socket " + i + " for the first time...");
+                            sockets[i] = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                                    Integer.parseInt(REMOTE_PORTS[i]));
+                        }
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Some exception when trying to create sockets");
                     Log.e(TAG, Log.getStackTraceString(e));
                 }
+                sequence++;
+                MessageWrapper[] replies = new MessageWrapper[5];
+                for (int j = 0; j < sockets.length; j++) {
+                    Log.v(TAG, "Sending " + msgs[0] + " through socket ");
+                    try {
+
+                        //write the message with proposed priority to everyone
+                        String msgToSend = msgs[0];
+                        double priority = sequence + Double.parseDouble(myPort) / 10000;
+                        MessageWrapper messageWrapper = new MessageWrapper(msgToSend, priority, MessageWrapper.TYPE_MESSAGE);
+                        messageWrapper.setIsReady(false);
+                        ObjectOutputStream objectOutputStream = new ObjectOutputStream(sockets[j].getOutputStream());
+                        objectOutputStream.writeObject(messageWrapper);
+                        objectOutputStream.flush();
+
+                        //wait for and read the reply
+
+                        ObjectInputStream objectInputStream = new ObjectInputStream(sockets[j].getInputStream());
+                        try {
+                            replies[j] = (MessageWrapper) objectInputStream.readObject();
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                    } catch (UnknownHostException e) {
+                        Log.e(TAG, "ClientTask UnknownHostException");
+                    } catch (IOException e) {
+                        Log.e(TAG, "ClientTask socket IOException");
+                        Log.e(TAG, Log.getStackTraceString(e));
+                    }
+                }
+
+                //process the replies - choose the highest reply
+                double maxPriority = 0;
+                for (int i = 0; i < replies.length; i++) {
+
+                }
+
+
+                return null;
             }
-            return null;
         }
     }
+
     private Uri buildUri(String scheme, String authority) {
         Uri.Builder uriBuilder = new Uri.Builder();
         uriBuilder.authority(authority);
--- GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/MessageWrapper.java
+++ GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/MessageWrapper.java
@@ -8,8 +8,21 @@ import java.util.Comparator;
  */
 
 class MessageWrapper implements Serializable {
-    private String message;
+
+
+    /**
+     * if type == MessageWrapper.MESSAGE_TYPE, data holds the message
+     * if type == MessageWrapper.REPLY_TYPE, reply type holds the initial sequence number
+     * if type == MessageWrapper.ACK_TYPE, reply type holds the final sequence number
+     */
+    private String data;
     private Double priority;
+    private int type;
+    private boolean isReady;
+
+    public static final int TYPE_MESSAGE = 1;
+    public static final int TYPE_REPLY =  2;
+    public static final int TYPE_ACK = 3;
 
     static Comparator<MessageWrapper> messageWrapperComparator = new Comparator<MessageWrapper>() {
 
@@ -25,31 +38,51 @@ class MessageWrapper implements Serializable {
         }
     };
 
-    public MessageWrapper(String message, Double priority) {
-        super();
-        this.message = message;
+    public void setPriority(Double priority) {
         this.priority = priority;
     }
 
-    public String getMessage() {
-        return message;
+    public MessageWrapper(String data, Double priority, int type) {
+        this.data = data;
+        this.priority = priority;
+        this.type = type;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 
     public Double getPriority() {
         return priority;
     }
 
-    public void setPriority(Double priority) {
-        this.priority = priority;
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public boolean isReady() {
+        return isReady;
+    }
+
+    public void setIsReady(boolean isReady) {
+        this.isReady = isReady;
     }
 
     @Override
     public String toString() {
-        return "MessageObject [message=" + message + ", priority=" + priority + "]";
+        return "MessageWrapper{" +
+                "type=" + type +
+                ", priority=" + priority +
+                ", data='" + data + '\'' +
+                ", isReady=" + isReady +
+                '}';
     }
-
 }
